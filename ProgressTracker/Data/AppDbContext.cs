723ada3ce653c7e7798b29489494b1140448ddda
Ankit <iamankitkumar546@gmail.com>
2025-04-24 23:32:19 +0530

    Added dbcontext and migration

---
+++ ProgressTracker/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProgressTracker.Models;
+
+namespace ProgressTracker.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<TaskItem> Tasks { get; set; }
+        public DbSet<Habit> Habits { get; set; }
+
+    }
+}
--- ProgressTracker/Program.cs
+++ ProgressTracker/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProgressTracker.Data;
+using ProgressTracker.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddAuthentication().AddJwtBearer();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
